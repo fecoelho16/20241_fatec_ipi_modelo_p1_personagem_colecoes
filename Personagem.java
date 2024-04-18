8acd0f86900daede6867d53c4d09bedf2b9934a1
Fernando Magalhães <contatofernandomc@gmail.com>
2024-04-18 00:47:06 -0300

    condicionarefeiçãoaexistenciadeitens

--- Personagem.java
+++ Personagem.java
@@ -1,108 +1,112 @@
 import java.util.Random;
 
 public class Personagem{
-  //variáveis de instância
-  String nome;
-  int energia;
-  private int fome;
-  private int sono;
-  private VetorDinamico<String> itens;
+    //variáveis de instância
+    String nome;
+    int energia;
+    private int fome;
+    private int sono;
+    private VetorDinamico<String> itens;
 
-  //esse é o construtor padrão
-  //criado automaticamente pelo compilador, ainda que não seja escrito explicitamente
-  Personagem(){
-    nome = null;
-    energia = 10;
-    fome = 0;
-    sono = 0;
-    itens = new VetorDinamico<String>(4);
-    Random random = new Random();
-    for (int i = 0; i < 4; i++) {
-      switch (random.nextInt(5)) {
-        case 0:
-          itens.adicionar("Javali selvagem");
-          break;
-        case 1:
-          itens.adicionar("Amora");
-          break;
-        case 2:
-          itens.adicionar("Batata frita");
-          break;
-        case 3:
-          itens.adicionar("Coelho");
-          break;
-        case 4:
-          itens.adicionar("Erva medicinal");
-          break;
-      }
+    //esse é o construtor padrão
+    //criado automaticamente pelo compilador, ainda que não seja escrito explicitamente
+    Personagem(){
+        nome = null;
+        energia = 10;
+        fome = 0;
+        sono = 0;
+        itens = new VetorDinamico<String>(4);
+        Random random = new Random();
+        for (int i = 0; i < 4; i++) {
+            switch (random.nextInt(5)) {
+                case 0:
+                    itens.adicionar("Javali selvagem");
+                    break;
+                case 1:
+                    itens.adicionar("Amora");
+                    break;
+                case 2:
+                    itens.adicionar("Batata frita");
+                    break;
+                case 3:
+                    itens.adicionar("Coelho");
+                    break;
+                case 4:
+                    itens.adicionar("Erva medicinal");
+                    break;
+            }
+        }
     }
-  }
 
-  //construtor personalizado
-  //o que viabiliza a sua existência é a sobrecarga de construtores
-  Personagem(int energia, int fome, int sono){
-    if (energia >= 0 && energia <= 10)
-      this.energia = energia;
-    if (fome >= 0 && fome <= 10)
-      this.fome = fome;
-    if (sono >= 0 && sono <= 10)
-      this.sono = sono;
-  }
+    //construtor personalizado
+    //o que viabiliza a sua existência é a sobrecarga de construtores
+    Personagem(int energia, int fome, int sono){
+        if (energia >= 0 && energia <= 10)
+            this.energia = energia;
+        if (fome >= 0 && fome <= 10)
+            this.fome = fome;
+        if (sono >= 0 && sono <= 10)
+            this.sono = sono;
+    }
 
-  void cacar(){
-    if(energia <= 0) {
-      System.out.printf("%s morreu... Fim de jogo!\n", nome);
-      System.exit(0);
+    void cacar(){
+        if(energia <= 0) {
+            System.out.printf("%s morreu... Fim de jogo!\n", nome);
+            System.exit(0);
+        }
+        if(energia >= 2){
+            System.out.printf("%s esta cacando...\n", nome);
+            energia -= 2; // energia = energia - 2;
+            Random random = new Random();
+            String[] itensDisponiveis = {"Javali selvagem", "Amora", "Batata frita", "Coelho", "Erva medicinal"};
+            itens.adicionar(itensDisponiveis[random.nextInt(5)]);
+}
+        else{
+            System.out.printf("%s sem energia para cacar...\n", nome);
+        }
+        fome = Math.min(fome + 1, 10);
+        sono = sono < 10 ? sono + 1 : sono;
     }
-    if(energia >= 2){
-      System.out.printf("%s esta cacando...\n", nome);
-      energia -= 2; // energia = energia - 2;
-      Random random = new Random();
-      String[] itensDisponiveis = {"Javali selvagem", "Amora", "Batata frita", "Coelho", "Erva medicinal"};
-      itens.adicionar(itensDisponiveis[random.nextInt(5)]);
-    }
-    else{
-      System.out.printf("%s sem energia para cacar...\n", nome);
-    }
-    fome = Math.min(fome + 1, 10);
-    sono = sono < 10 ? sono + 1 : sono;
-  }
 
-  void comer() {
-    //se tiver fome
-    //comer e reduzir o valor de fome de 1
-    //aumentar o valor de energia de 1
-    //caso contrario
-    //so vai avisar que esta sem fome
-    switch(fome){
-      case 0:
-        System.out.printf("%s sem fome....\n", nome);
-        break;
-      default:
-        System.out.printf("%s comendo...\n", nome);
-        --fome;
-        energia = (energia == 10 ? energia : energia + 1);
-    }
-  }
+    void comer() {
+        // se não houver itens, não é possível comer
+        if (itens.tamanho() == 0) {
+            System.out.printf("%s não tem itens para comer.\n", nome);
+            return;
+        }
 
-  void dormir(){
-    if(sono >= 1){
-      System.out.printf("%s esta dormindo...\n", nome);
-      sono -= 1;
-      energia = Math.min(energia + 1, 10);
-    }
-    else{
-      System.out.printf("%s sem sono...\n", nome);
-    }
-  }
+        // come o último item da coleção
+        String ultimoItem = itens.getElementos()[itens.tamanho() - 1];
+        System.out.printf("%s come %s.\n", nome, ultimoItem);
 
-  public String toString(){
-    StringBuilder sb = new StringBuilder();
-    sb.append(nome).append(": (e:").append(energia).append(", f:").append(fome).append(", s:").append(sono).append(")\n");
-    sb.append("Itens: ");
-    for (int i = 0; i < itens.tamanho(); i++) {
-      sb.append(itens.getElementos()[i]).append(" ");
+        // remove o último item da coleção
+        itens.removerNoFinal();
+
+        // reduz o valor de fome em 1
+        fome--;
+
+        // aumenta o valor de energia em 1
+        energia = Math.min(energia + 1, 10);
     }
-    return sb.toString();
-  }
+
+    void dormir(){
+        if(sono >= 1){
+            System.out.printf("%s esta dormindo...\n", nome);
+            sono -= 1;
+            energia = Math.min(energia + 1, 10);
+        }
+        else{
+            System.out.printf("%s sem sono...\n", nome);
+        }
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(nome).append(": (e:").append(energia).append(", f:").append(fome).append(", s:").append(sono).append(")\n");
+        sb.append("Itens: ");
+        for (int i = 0; i < itens.tamanho(); i++) {
+            sb.append(itens.getElementos()[i]).append(" ");
+        }
+        return sb.toString();
+    }
 }
--- VetorDinamico.java
+++ VetorDinamico.java
@@ -1,79 +1,85 @@
 import java.util.Arrays;
 
 public class VetorDinamico<T> {
-  //variáveis de instância
-  private int qtde;
-  private int cap;
-  private T[] elementos;
+    //variáveis de instância
+    private int qtde;
+    private int cap;
+    private T[] elementos;
 
-  //variável de classe
-  private static final int CAP_MINIMA = 4;
+    //variável de classe
+    private static final int CAP_MINIMA = 4;
 
-  //construtor
-  VetorDinamico(){
-    cap = CAP_MINIMA;
-    qtde = 0;
-    elementos = (T[]) new Object[cap];
-  }
-
-  VetorDinamico(int capMinima){
-    double aux = capMinima;
-    while (aux >= 2)
-      aux /= 2;
-    cap = aux == 1 && capMinima > 3 ? capMinima : CAP_MINIMA;
-  }
-
-  //método adicionar
-  void adicionar(T elemento){
-    //se estiver cheio, redimensionar antes de adicionar
-    if(estaCheio()) redimensionar();
-    elementos[qtde] = elemento;
-    qtde++;
-  }
-
-  //método redimensionar
-  private void redimensionar(){
-    //alocar um vetor com o dobro da capacidade atual chamado auxiliar
-    T[] aux = (T[]) new Object[cap * 2];
-
-    //copiar todo mundo do vetor elementos para o vetor auxiliar
-    for(int i = 0; i < cap; i++){
-      aux[i] = elementos[i];
+    //construtor
+    VetorDinamico(){
+        cap = CAP_MINIMA;
+        qtde = 0;
+        elementos = (T[]) new Object[cap];
     }
 
-    //ajustar a capacidade para que ela tenha o valor novo, dobrado
-    cap *= 2;
-
-    //ajustar o ponteiro elementos para que ele aponte para o novo vetor
-    elementos = aux;
-  }
-
-  //método estaCheio
-  boolean estaCheio(){
-    //decidir se o vetor está cheio ou não olhando para cap e qtde
-    if(qtde == cap)
-      return true;
-    return false;
-  }
-
-  //método tamanho
-  int tamanho(){
-    return qtde;
-  }
-
-  //método getElementos
-  T[] getElementos(){
-    return Arrays.copyOf(elementos, cap);
-  }
-
-  public String toString(){
-    StringBuilder sb = new StringBuilder();
-    sb.append("Qtde: ").append(qtde).append("\n");
-    sb.append("Cap: ").append(cap).append("\n");
-    sb.append("Elementos: ");
-    for (int i = 0; i < qtde; i++){
-      sb.append(elementos[i]).append(" ");
+    VetorDinamico(int capMinima){
+        double aux = capMinima;
+        while (aux >= 2)
+            aux /= 2;
+        cap = aux == 1 && capMinima > 3 ? capMinima : CAP_MINIMA;
     }
-    return sb.toString();
-  }
+
+    //método adicionar
+    void adicionar(T elemento){
+        //se estiver cheio, redimensionar antes de adicionar
+        if(estaCheio()) redimensionar();
+        elementos[qtde] = elemento;
+        qtde++;
+    }
+
+    //método redimensionar
+    private void redimensionar(){
+        //alocar um vetor com o dobro da capacidade atual chamado auxiliar
+        T[] aux = (T[]) new Object[cap * 2];
+
+        //copiar todo mundo do vetor elementos para o vetor auxiliar
+        for(int i = 0; i < cap; i++){
+            aux[i] = elementos[i];
+        }
+
+        //ajustar a capacidade para que ela tenha o valor novo, dobrado
+        cap *= 2;
+
+        //ajustar o ponteiro elementos para que ele aponte para o novo vetor
+        elementos = aux;
+    }
+
+    //método estaCheio
+    boolean estaCheio(){
+        //decidir se o vetor está cheio ou não olhando para cap e qtde
+        if(qtde == cap)
+            return true;
+        return false;
+    }
+
+    //método tamanho
+    int tamanho(){
+        return qtde;
+    }
+
+    //método getElementos
+    T[] getElementos(){
+        return Arrays.copyOf(elementos, cap);
+    }
+
+    //método removerNoFinal
+    void removerNoFinal(){
+        if(qtde > 0)
+            qtde--;
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Qtde: ").append(qtde).append("\n");
+        sb.append("Cap: ").append(cap).append("\n");
+        sb.append("Elementos: ");
+        for (int i = 0; i < qtde; i++){
+            sb.append(elementos[i]).append(" ");
+        }
+        return sb.toString();
+    }
 }
